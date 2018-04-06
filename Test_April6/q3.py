13aeef38cc077455758a5d41f42631618013f2c5
karansingla06 <karansingla.06@gmail.com>
2018-04-06 15:58:03 +0530

    Create q3.py

---
+++ Test_April6/q3.py
@@ -0,0 +1,34 @@
+#question3
+
+
+def merge(a,b):
+    """ Function to merge two arrays """
+    c = []
+    while len(a) != 0 and len(b) != 0:
+        if a[0] < b[0]:
+            c.append(a[0])
+            a.remove(a[0])
+        else:
+            c.append(b[0])
+            b.remove(b[0])
+    if len(a) == 0:
+        c += b
+    else:
+        c += a
+    return c
+
+# Code for merge sort
+
+def mergesort(x):
+    if len(x) == 0 or len(x) == 1:
+        return x
+    else:
+        middle = len(x)/2
+        a = mergesort(x[:middle])
+        b = mergesort(x[middle:])
+        return merge(a,b)
+
+
+li= [12,6,48,37,88,31]
+li.sort()
+print(li)
