432d5a1da3a5d36748a92f94ecf9d4bd5341ae06
karansingla06 <karansingla.06@gmail.com>
2018-04-06 12:04:24 +0530

    Create linearRegression.py

---
+++ Algos_ml/linearRegression.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+def estimate_coef(x, y):
+    # number of observations/points
+    n = np.size(x)
+    
+    # mean of x and y vector
+    m_x, m_y = np.mean(x), np.mean(y)
+
+    # calculating cross-deviation and deviation about x-- (xi-m_x)(yi-m_y)
+    SS_xy = np.sum(y*x - n*m_y*m_x)
+    SS_xx = np.sum(x*x - n*m_x*m_x)
+ 
+    # calculating regression coefficients
+    b_1 = SS_xy / SS_xx
+    b_0 = m_y - b_1*m_x
+ 
+    return(b_0, b_1)
+ 
+def plot_regression_line(x, y, b):
+    # plotting the actual points as scatter plot
+    plt.scatter(x, y, marker = "o", s = 30,alpha=0.5)
+ 
+    # predicted response vector
+    y_pred = b[0] + b[1]*x
+    #print("pred y is :  ",y_pred)
+ 
+    # plotting the regression line
+    plt.plot(x, y_pred,color="blue")
+ 
+    # putting labels
+    plt.xlabel('x')
+    plt.ylabel('y')
+ 
+    # function to show plot
+    plt.show()
+ 
+def main():
+    # observations
+    x = np.array([8,2,11,6,5,4,12,9,6,1])
+    y = np.array([3,10,3,6,8,12,1,4,9,14])
+ 
+    # estimating coefficients
+    b = estimate_coef(x, y)
+    print("Estimated coefficients:\nb_0 = {}  \
+          \nb_1 = {}".format(b[0], b[1]))
+ 
+    # plotting regression line
+    plot_regression_line(x, y, b)
+ 
+if __name__ == "__main__":
+    main()
